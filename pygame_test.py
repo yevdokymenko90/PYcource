609329b237c55ba3a0e1171150814c0e999163d3
yevdokymenko90 <yevdokymenko90@gmail.com>
2024-03-17 20:14:45 +0100

    fixed types

--- pygame_test.py
+++ pygame_test.py
@@ -2,4 +2,3 @@ import pygame
 
 pygame.init()
 
-
--- types_test.py
+++ types_test.py
@@ -3,16 +3,16 @@ str2 = "string with double quotes"
 print(str1)
 print(str2)
 
-int = 10
-print(int)
+int_var = 10
+print(int_var)
 
 bool1 = True
 bool2 = False
 print(bool1)
 print(bool2)
 
-list = [1, 2, 3, 4, 5]
-print(list) 
+list_var = [1, 2, 3, 4, 5]
+print(list_var) 
 
-dict = {'name': 'John', 'age': 25}
-print(dict)
+dict_var = {'name': 'John', 'age': 25}
+print(dict_var)
